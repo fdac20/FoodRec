7e0b61a9782b124ec8c52be042fad01b9add4b83
dnguye201 <dnguye18@vols.utk.edu>
2020-10-22 10:42:26 -0400

    Created a dataframe for asian restaurants.
    Wrote some code to list all the genres of food in the restaurants dataframe. Then I used the categories of all asian restaurants and put it in a dataframe. Now I should have a dataframe of ALL asian restaurants with their review count, stars, etc. Now we just need to do the same thing for every other genre.

--- FoodRec.py
+++ FoodRec.py
@@ -9,9 +9,15 @@ df_b = df_b[df_b['is_open'] == 1] #Only need the restaurants that are open
 drop_columns = ['hours','is_open', 'address', 'city', 'state', 'postal_code', 'latitude', 'longitude'] #Dropping these categories cause we dont need it. Could be more.
 df_b = df_b.drop(drop_columns, axis=1)
 
-#This sets the output to show only one output. I just wanted to see the different column names.
+#This sets the output to show only one row. I just wanted to see the different column names.
 #pd.set_option("display.max_rows", 1, "display.max_columns", None)
 
 #A dataframe containing ONLY places that have the restaurant category.
 restaurants = df_b[df_b['categories'].str.contains('Restaurants', case = False, na = False)]
-print(restaurants)
+
+#This will let you see ALL the restaurant categories(Asian, Mexican, Fast Food, etc.)
+#df_explode = restaurants.assign(categories = restaurants.categories.str.split(', ')).explode('categories')
+#print(df_explode.categories.value_counts())
+
+#This should return a dataframe containing any and all asian restaurants. Rinse and repeat for all other groups.
+asian_restaurants = restaurants[restaurants['categories'].str.contains('Chinese|Japanese|Sushi Bars|Asian Fusion|Indian|Thai|Vietnamese|Korean|Bubble Tea|Ramen|Dim Sum|Taiwanese|Hot Pot|Pan Asian|Szechuan|Malaysian|Indonesian', case=False, na=False)]
